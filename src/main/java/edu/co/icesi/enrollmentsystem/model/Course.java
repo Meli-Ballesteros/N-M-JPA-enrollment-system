6c647785333ec8684ed0e32db13ee56170c2b8ba
Melissa Ballesteros <1061684538@u.icesi.edu.co>
2026-09-08 17:14:15 -0500

    creacion de la clase Course

--- src/main/java/edu/co/icesi/enrollmentsystem/model/Course.java
+++ src/main/java/edu/co/icesi/enrollmentsystem/model/Course.java
@@ -1,4 +1,31 @@
 package edu.co.icesi.enrollmentsystem.model;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "courses")
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    
+    public Course() {
+    }
+
+    public Course(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
